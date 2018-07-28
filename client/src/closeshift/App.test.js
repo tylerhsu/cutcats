26c052c6100efdf7385f676409f57f9cb660eb40
Tyler Hsu <tyler.l.hsu@gmail.com>
2018-07-28 11:46:55 -0500

    remove .only

--- client/src/closeshift/App.test.js
+++ client/src/closeshift/App.test.js
@@ -1,7 +1,7 @@
 import { App } from './App';
 import { shallow } from 'enzyme';
 
-describe.only('close shift App', function() {
+describe('close shift App', function() {
     it('renders without crashing', function() {
         shallow(<App />);
     });
