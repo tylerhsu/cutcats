ecce821b5f991e679acfab4fc67a4cabe857435f
Tyler Hsu <tyler.l.hsu@gmail.com>
2018-07-28 12:09:29 -0500

    add eslint

--- client/src/closeshift/CloseShiftForm.js
+++ client/src/closeshift/CloseShiftForm.js
@@ -3,60 +3,62 @@ import Uploader from './Uploader';
 import ShiftDetails from './ShiftDetails';
 
 export default class CloseShiftForm extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            importFile: null,
-            uploaderValidationErrors: null,
-            uploaderError: null
-        };
-    }
+    this.state = {
+      importFile: null,
+      uploaderValidationErrors: null,
+      uploaderError: null
+    };
+  }
 
-    handleValidationSuccess = (file) => {
-        this.setState({
-            importFile: file,
-            uploaderValidationErrors: null,
-            uploaderError: null
-        });
-    }
+  handleValidationSuccess = (file) => {
+    this.setState({
+      importFile: file,
+      uploaderValidationErrors: null,
+      uploaderError: null
+    });
+  }
 
-    handleValidationFailure = (errors) => {
-        this.setState({
-            importFile: null,
-            uploaderValidationErrors: errors,
-            uploaderError: null
-        });
-    }
+  handleValidationFailure = (errors) => {
+    this.setState({
+      importFile: null,
+      uploaderValidationErrors: errors,
+      uploaderError: null
+    });
+  }
 
-    handleUploaderError = (err) => {
-        this.setState({
-            importFile: null,
-            uploaderValidationErrors: null,
-            uploaderError: err.message
-        });
-    }
-    
-    render() {
-        return (
-            <div>
-              <div>
-                <h1>1. Import ride data</h1>
-                <Uploader
-                    validationUrl='/api/rides/import?save=false'
-                    onValidationSuccess={this.handleValidationSuccess}
-                    onValidationFailure={this.handleValidationFailure}
-                    onError={this.handleUploaderError}
-                />
-              </div>
-              <div>
-                <fieldset disabled={!this.state.importFile} style={{ color: !this.state.importFile ? 'lightgray' : null }}>
-                  <h1>2. Enter shift details</h1>
-                  <ShiftDetails />
-                  <button onClick={this.submit}>Close shift</button>
-                </fieldset>
-              </div>
-            </div>
-        );
-    }
+  handleUploaderError = (err) => {
+    this.setState({
+      importFile: null,
+      uploaderValidationErrors: null,
+      uploaderError: err.message
+    });
+  }
+
+  submit
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>1. Import ride data</h1>
+          <Uploader
+            validationUrl='/api/rides/import?save=false'
+            onValidationSuccess={this.handleValidationSuccess}
+            onValidationFailure={this.handleValidationFailure}
+            onError={this.handleUploaderError}
+          />
+        </div>
+        <div>
+          <fieldset disabled={!this.state.importFile} style={{ color: !this.state.importFile ? 'lightgray' : null }}>
+            <h1>2. Enter shift details</h1>
+            <ShiftDetails />
+            <button onClick={this.submit}>Close shift</button>
+          </fieldset>
+        </div>
+      </div>
+    );
+  }
 }
