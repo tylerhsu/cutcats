ccd0c9f974af5c020dc06193e2fc700671e04dd8
Tyler Hsu <tyler.l.hsu@gmail.com>
2018-08-01 10:55:24 -0500

    change indenting

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,75 +1,75 @@
 if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config();
+  require('dotenv').config();
 }
 
 const apiPort = parseInt(process.env.PORT) || 3000;
 
 module.exports = {
-    options: {
-        source: 'client/src',
-        output: 'client/build',
-        mains: {
-            'index': 'index',
-            'couriers': 'couriers',
-            'closeshift': 'closeshift',
-            'welcome': 'welcome',
-            'quickexport': 'quickexport',
-            'rides': 'rides',
-            'payroll': 'payroll',
-            'invoices': 'invoices',
-            'clients': 'clients',
-        },
-        tests: 'client'
+  options: {
+    source: 'client/src',
+    output: 'client/build',
+    mains: {
+      'index': 'index',
+      'couriers': 'couriers',
+      'closeshift': 'closeshift',
+      'welcome': 'welcome',
+      'quickexport': 'quickexport',
+      'rides': 'rides',
+      'payroll': 'payroll',
+      'invoices': 'invoices',
+      'clients': 'clients',
     },
-    use: [
-        [
-            '@neutrinojs/react',
+    tests: 'client'
+  },
+  use: [
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'cutcats'
+        },
+        devServer: {
+          staticOptions: {
+            redirect: false
+          },
+          historyApiFallback: {
+            rewrites: [
+              {
+                from: /.*/,
+                to: context => (context.match + '.html')
+              }
+            ]
+          },
+          proxy: [{
+            context: ['/api', '/auth'],
+            target: 'http://localhost:' + apiPort,
+          }],
+          port: apiPort + 1
+        },
+        style: {
+          test: /\.s?css$/,
+          loaders: [
             {
-                html: {
-                    title: 'cutcats'
-                },
-                devServer: {
-                    staticOptions: {
-                        redirect: false
-                    },
-                    historyApiFallback: {
-                        rewrites: [
-                            {
-                                from: /.*/,
-                                to: context => (context.match + '.html')
-                            }
-                        ]
-                    },
-                    proxy: [{
-                        context: ['/api', '/auth'],
-                        target: 'http://localhost:' + apiPort,
-                    }],
-                    port: apiPort + 1
-                },
-                style: {
-                    test: /\.s?css$/,
-                    loaders: [
-                        {
-                            loader: 'sass-loader',
-                            useId: 'sass'
-                        }
-                    ]
-                }
+              loader: 'sass-loader',
+              useId: 'sass'
             }
-        ],
-        [
-            '@neutrinojs/mocha',
-            {
-                exit: true
-            }
-        ],
-        neutrino => neutrino.config
-            .entry('vendor')
-            .add('babel-polyfill')
-            .add('react')
-            .add('react-dom')
-            .add('prop-types')
-            .add('axios')
-            .add('reactstrap')
-    ]
+          ]
+        }
+      }
+    ],
+    [
+      '@neutrinojs/mocha',
+      {
+        exit: true
+      }
+    ],
+    neutrino => neutrino.config
+      .entry('vendor')
+      .add('babel-polyfill')
+      .add('react')
+      .add('react-dom')
+      .add('prop-types')
+      .add('axios')
+      .add('reactstrap')
+  ]
 };
